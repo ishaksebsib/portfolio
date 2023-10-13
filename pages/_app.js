5ba4e610d9200b762c6ce2fc3d868c9d10dc5e82
ishaksebsib <ishaksebsib@gmail.com>
2023-10-13 15:47:09 +0300

    loading screen optimized

--- pages/_app.js
+++ pages/_app.js
@@ -1,27 +1,17 @@
 import "../styles/globals.css";
 import { ThemeProvider } from "next-themes";
 import Head from "next/head";
-import { useEffect, useState } from "react";
-import Loading from "../components/Loading";
 
 function MyApp({ Component, pageProps }) {
-  const [loading, setLoading] = useState(true);
-
-  useEffect(() => {
-    setTimeout(() => setLoading(false), 4000);
-  }, []);
   return (
     <>
       <Head>
-        <title>Loading</title>
+        <title>Ishak Sebsib | Portfolio Site</title>
       </Head>
-      {loading ? (
-        <Loading />
-      ) : (
-        <ThemeProvider enableSystem={true} attribute="class">
-          <Component {...pageProps} />
-        </ThemeProvider>
-      )}
+
+      <ThemeProvider enableSystem={true} attribute="class">
+        <Component {...pageProps} />
+      </ThemeProvider>
     </>
   );
 }
--- pages/index.js
+++ pages/index.js
@@ -1,30 +1,47 @@
+import { useEffect, useState } from "react";
+import Head from "next/head";
+import Loading from "../components/Loading";
 import NavBar from "../components/NavBar";
 import SocailMedias from "../components/SocailMedias";
-import UpAndDownArrow from "../components/UpAndDownArrow";
+// import UpAndDownArrow from "../components/UpAndDownArrow";
 import HomePage from "../components/pages/home/HomePage";
 import WorkPage from "../components/pages/home/WorkPage";
 import AboutMe from "../components/pages/home/AboutMe";
 import GetInTouch from "../components/pages/home/GetInTouch";
 import Footer from "../components/Footer";
 import Skills from "../components/pages/home/Skills";
-import Head from "next/head";
 
 export default function Home() {
+  const [loading, setLoading] = useState(true);
+  useState;
+
+  useEffect(() => {
+    // Simulate a delay to showcase the loading screen
+    const timeout = setTimeout(() => {
+      setLoading(false);
+    }, 2000);
+
+    return () => clearTimeout(timeout); // Cleanup the timeout on unmount
+  }, []);
+
   return (
-    <div className="fitscreen">
-      <Head>
-        <title>Portfolio</title>
-      </Head>
-      <div className=" bg-bgcolor w-full ">
-        <NavBar />
-        <SocailMedias />
-        <HomePage />
-        <WorkPage />
-        <Skills />
-        <AboutMe />
-        <GetInTouch />
-        <Footer />
-      </div>
-    </div>
+    <>
+      {loading && <Loading />}
+
+      {!loading && (
+        <div className="fitscreen">
+          <div className=" bg-bgcolor w-full ">
+            <NavBar />
+            <SocailMedias />
+            <HomePage />
+            <WorkPage />
+            <Skills />
+            <AboutMe />
+            <GetInTouch />
+            <Footer />
+          </div>
+        </div>
+      )}
+    </>
   );
 }
