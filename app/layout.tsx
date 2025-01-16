5d64e923d39cb20ce0dd09e3c2db188ef39586dc
ishaksebsib <ishaksebsib@gmail.com>
2025-01-16 19:13:55 +0300

    [ENH]: skill section craft v2

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,34 +1,47 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Geist, Geist_Mono, Roboto, Inter as FontSans } from "next/font/google";
 import "./globals.css";
 
+const fontSans = FontSans({
+	subsets: ["latin"],
+	variable: "--font-sans",
+});
+
 const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
+	variable: "--font-geist-sans",
+	subsets: ["latin"],
 });
 
 const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
+	variable: "--font-geist-mono",
+	subsets: ["latin"],
+});
+
+const roboto = Roboto({
+	subsets: ["latin"],
+	variable: "--font-roboto",
+	weight: ["400", "700"],
+	style: ["normal", "italic"],
+	display: "swap",
 });
 
 export const metadata: Metadata = {
-  title: "Ishak Sebsib Portfolio",
-  description: "Ishak Sebsib Portfolio Website",
+	title: "Ishak Sebsib Portfolio",
+	description: "Ishak Sebsib Portfolio Website",
 };
 
 export default function RootLayout({
-  children,
+	children,
 }: Readonly<{
-  children: React.ReactNode;
+	children: React.ReactNode;
 }>) {
-  return (
-    <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        {children}
-      </body>
-    </html>
-  );
+	return (
+		<html lang="en">
+			<body
+				className={`${geistSans.variable} ${geistMono.variable} ${roboto.variable} ${fontSans.variable} antialiased`}
+			>
+				{children}
+			</body>
+		</html>
+	);
 }
--- components/sections/Skills.tsx
+++ components/sections/Skills.tsx
@@ -6,6 +6,12 @@ import {
   useScroll,
   useMotionValueEvent,
 } from "framer-motion";
+import Svg from "../utils/Svg";
+import GoSvg from "../utils/svg/GoSvg";
+import RustSvg from "../utils/svg/RustSvg";
+import TsSvg from "../utils/svg/TsSvg";
+import PythonSvg from "../utils/svg/PythonSvg";
+import ReactSvg from "../utils/svg/ReactSvg";
 
 const Skills = () => {
   const { scrollYProgress } = useScroll();
@@ -39,11 +45,86 @@ const Skills = () => {
               opacity: 1,
             }}
             transition={{
-              duration: 0.4,
+              duration: 0.3,
             }}
-            className={`h-5/6 w-4/5 bg-foreground flex items-center justify-center rounded-xl shadow-slate-100 shadow-sm hover:shadow-slate-200 hover:shadow-xl transition-all duration-300 transform `}
+            className={`h-5/6 w-4/5 bg-white flex items-center justify-center rounded-xl shadow-slate-400 shadow-sm hover:shadow-slate-200 hover:shadow-xl transition-all duration-300 transform `}
           >
-            <h1>Hello world</h1>
+            <div className="w-full h-auto full flex flex-col justify-start items-start p-6 gap-8 md:p-8 lg:p-12">
+              {/* Heading Section */}
+              <div className="flex flex-col items-center justify-center text-center">
+                <h1 className="font-geist text-xl text-darkfont font-extrabold sm:text-2xl md:text-3xl lg:text-4xl">
+                  Langs I work with
+                </h1>
+                <p className="text-lightdarkfont font-rob font-thin mt-4 text-sm sm:text-base md:text-lg lg:text-xl max-w-full md:max-w-[60%]">
+                  Love to learn new things and master what I know. Currently
+                  working on a prototyping studio making apps and websites that
+                  are fun to play with.
+                </p>
+              </div>
+
+              {/* Skills Section */}
+              <div className="w-full h-80 sm:h-72 md:h-80 lg:h-96 rounded-xl flex flex-col items-center justify-center gap-10">
+                {/*
+                 *bottom small icons
+                 */}
+                <div className="w-full flex justify-center items-center gap-8 ">
+                  <div className="w-full max-w-20">
+                    <GoSvg />
+                  </div>
+                  <div className="w-full max-w-20">
+                    <TsSvg />
+                  </div>
+                  <div className="w-full max-w-20">
+                    <RustSvg />
+                  </div>
+                  <div className="w-full max-w-20">
+                    <PythonSvg />
+                  </div>
+                  <div className="w-full max-w-20">
+                    <ReactSvg />
+                  </div>
+                </div>
+
+                <div className="w-full flex justify-center gap-10">
+                  <div className="w-full max-w-32">
+                    <GoSvg />
+                  </div>
+                  <div className="w-full max-w-32">
+                    <TsSvg />
+                  </div>
+                  <div className="w-full max-w-32">
+                    <RustSvg />
+                  </div>
+                  <div className="w-full max-w-32">
+                    <PythonSvg />
+                  </div>
+                  <div className="w-full max-w-32">
+                    <ReactSvg />
+                  </div>
+                </div>
+
+                {/*
+                 *bottom small icons
+                 */}
+                <div className="w-full flex justify-center items-center gap-8 ">
+                  <div className="w-full max-w-20">
+                    <GoSvg />
+                  </div>
+                  <div className="w-full max-w-20">
+                    <TsSvg />
+                  </div>
+                  <div className="w-full max-w-20">
+                    <RustSvg />
+                  </div>
+                  <div className="w-full max-w-20">
+                    <PythonSvg />
+                  </div>
+                  <div className="w-full max-w-20">
+                    <ReactSvg />
+                  </div>
+                </div>
+              </div>
+            </div>
           </motion.div>
         </AnimatePresence>
       </section>
